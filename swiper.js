2fe087f0f4f37c62e440b3eb87c8fc64242d600e
Gunel Azizova <111835072+LittleSmile05@users.noreply.github.com>
2024-01-26 11:48:55 +0400

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,24 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+slidesPerView: 3,
+spaceBetween: 30,
+freeMode: true,
+pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+autoplay: {
+delay: 1500, 
+},
+});
